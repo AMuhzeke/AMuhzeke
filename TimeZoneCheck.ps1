a80014581747440db297b38bde8b7720ff75f834
AMuhzeke <jimmy@gmail.com>
2023-11-19 13:25:12 -0500

    TimeZoneCheck Powershell
    powershell script to check the time zone.

---
+++ TimeZoneCheck.ps1
@@ -0,0 +1,17 @@
+﻿$TZone = (Get-WMIObject -Class Win32_TimeZone).Bias
+If ($TZone -eq 720)
+  {
+  Write-Host "You are on the opposite side of the world from Greenwich"
+  }
+  ElseIf ($TZone.Bias -gt 0)
+  {
+  Write-Host "You are currently east of Greenwich"
+  }
+  ElseIf ($TZone.Bias -lt 0)
+  {
+  Write-Host "You are currently west of Greenwich"
+  }
+  Else
+  {
+  Write-Host "You must be in Greenwich"
+  }
